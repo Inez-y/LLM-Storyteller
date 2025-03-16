1aa41d96c851a1333d15f6580774b27d95ba80ff
Inez Yoon <inezyoon@Inezs-MacBook-Air.local>
2025-03-16 01:24:09 -0700

    Change directory

--- js/login.js
+++ js/login.js
@@ -25,10 +25,10 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
         // Check if the user is an admin
         if (result.isAdmin === true) {
           // Redirect to the admin dashboard
-          window.location.href = './dashboard.html';
+          window.location.href = 'html/dashboard.html';
         } else {
           // Redirect to the landing page
-          window.location.href = './landing.html';
+          window.location.href = 'html/landing.html';
         }
       } else {
         document.getElementById('message').textContent = 'Login failed. Please try again.';
