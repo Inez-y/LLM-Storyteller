c1ba632889a5d74cc00637cd981390b1b399b3c4
Inez Yoon <inezyoon@Inezs-MacBook-Air.local>
2025-03-16 20:31:01 -0700

    testing env

--- js/landing.js
+++ js/landing.js
@@ -22,8 +22,8 @@ async function getGPTResponse(text) {
     const key = OPENAI_KEY //process.env.OPENAI_KEY;
     const url = OPENAI_URL //process.env.OPENAI_URL; 
 
-    console.log("OPENAI_KEY:", key); // Debugging
-    console.log("OPENAI_URL:", url); // Debugging
+    console.log("OPENAI_KEY:", OPENAI_KEY); // Debugging
+    console.log("OPENAI_URL:", OPENAI_URL); // Debugging
 
     try {
         const response = await fetch(url, {
